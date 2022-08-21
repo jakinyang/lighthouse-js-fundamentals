1866fb29ca6f87b5e0598160064eb46781510eec
jakinyang <jakinyang@gmail.com>
2022-08-20 17:20:20 -0700

    More examples to conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,4 @@
+const raining = true;
 const cold = false;
 
 if (cold) {
@@ -18,4 +19,21 @@ if (temperature < 0) {
 
 console.log('text4');
 
+const isCit = true;
+const age = 22;
 
+if (isCit && age >= 18) {
+	console.log('You can vote.');
+}
+
+if (temperature < -40 || temperature > 40) {
+	console.log('How\'s this for climate change, bitch?!');
+} else {
+	console.log('Climate change is still real');
+}
+
+if (!raining) {
+	console.log('It\'s going to be a long drought, bud...');
+} else {
+	console.log('The rains are going to provide this year!');
+}
