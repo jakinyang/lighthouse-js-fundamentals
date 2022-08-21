8ddd53c3588220b7774410ea8034dfaa9471d543
jakinyang <jakinyang@gmail.com>
2022-08-20 17:10:33 -0700

    conditionals practice 1

---
+++ conditionals.js
@@ -0,0 +1,21 @@
+const cold = false;
+
+if (cold) {
+	console.log('text');
+} else {
+	console.log('alt text');
+}
+
+const temperature = 12;
+
+if (temperature < 0) {
+	console.log('text1');
+} else if (temperature < 15) {
+	console.log('text2');
+} else {
+	console.log('text3');
+}
+
+console.log('text4');
+
+
