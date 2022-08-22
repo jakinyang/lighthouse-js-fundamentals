558557d133741997b33c93d302a9ad4b375bd6f0
jakinyang <jakinyang@gmail.com>
2022-08-22 15:16:21 -0700

    committing modified hello.js with an array and loop function

--- hello.js
+++ hello.js
@@ -1,5 +1,24 @@
+/*
 const sayHello = function () {
   console.log('Hello, world');
 }
+*/
 
-sayHello();
+const sayHello = function (name) {
+  console.log(`Hello, ${name}`);
+}
+
+sayHello("Caliban");
+sayHello('Miranda');
+sayHello('ferdinand');
+
+let guestList = ['Ferdinand', 'Miranda', 'Caliban'];
+
+const greetingIter = function(array) {
+  for (i = 0; i < array.length; i++) {
+    sayHello(array[i]);
+  }
+  return 
+}
+
+console.log(greetingIter(guestList));
