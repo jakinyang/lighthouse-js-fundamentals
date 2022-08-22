9ab38a4a6a50620890aabdb352ea4d03233c6998
jakinyang <jakinyang@gmail.com>
2022-08-22 15:28:09 -0700

    committing modified hello.js and even.js

--- hello.js
+++ hello.js
@@ -15,10 +15,30 @@ sayHello('ferdinand');
 let guestList = ['Ferdinand', 'Miranda', 'Caliban'];
 
 const greetingIter = function(array) {
+  let greeting = '';
   for (i = 0; i < array.length; i++) {
-    sayHello(array[i]);
+    greeting += sayHello(array[i]);
   }
-  return 
+  return greeting;
 }
 
 console.log(greetingIter(guestList));
+
+const helloToConsole = function (name) {
+    console.log(`Hello, ${name}`);
+}
+
+helloToConsole('John');
+
+const returnHello = function(name) {
+  return `Hello, ${name}`;
+}
+
+const greeting = returnHello('John');
+console.log(greeting);
+
+const isEven = function(num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
